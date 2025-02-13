45a71786fc92d8a0be00862470b3fdeb97eb0dfd
DylanTigo <dylannoeltigomo@gmail.com>
2025-02-13 15:36:59 +0100

    Refactor Button2 component and update styles in globals.css;

--- components/ui/Button2.tsx
+++ components/ui/Button2.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import React from "react";
 import { motion } from "motion/react";
+import { ArrowRightIcon } from "lucide-react";
 
 const Button2 = ({ children }: { children: string }) => {
   const splitText = children.split("");
@@ -19,11 +20,11 @@ const Button2 = ({ children }: { children: string }) => {
   return (
     <Link href="#">
       <motion.div
-        className="btn group border border-zinc-950"
+        className="btn group border border-zinc-950 min-w-32 group"
         whileHover="hovered"
         initial="initial"
       >
-        <span className="btn-2-text h-6 flex flex-col">
+        <motion.span variants={{ initial: { x: 0 }, hovered: { x: "-5px" } }} transition={{ delay: 0.1, ease: "easeInOut" }} className="btn-2-text h-6 flex flex-col"> 
           <span className="w-fit h-fit">
             {splitText.map((char, index) => (
               <motion.span
@@ -54,7 +55,8 @@ const Button2 = ({ children }: { children: string }) => {
               </motion.span>
             ))}
           </span>
-        </span>
+        </motion.span>
+        <ArrowRightIcon className="size-4 stroke-zinc-950 absolute top-1/2 -translate-y-1/2 -right-4 group-hover:right-4 group-hover:-rotate-45 transition-elt" />
       </motion.div>
     </Link>
   );
